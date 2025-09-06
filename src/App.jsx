a4cfcee83b8797f3e095215ee3cd05bfe8a7e0c1
aagarcia1987 <adriang1987@gmail.com>
2025-09-05 21:09:48 -0300

    primera subida

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <div>
+      <h1>Hola</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  )
+}
